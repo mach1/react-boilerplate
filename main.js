53310090e6b796e20f366da405354c666e504a20
Jaan Lambert <jaan.lambert@gmail.com>
2015-11-01 13:14:01 +0100

    Added React and Babel

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello");
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, Link } from 'react-router'
+
+var App = React.createClass({
+  render() {
+    return <h1>Hello</h1>
+  }
+})
+
+render((
+  <Router>
+    <Route path="/" component={App}>
+ 
+    </Route>
+  </Router>
+), document.getElementById('container'))
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,22 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   }
 };
