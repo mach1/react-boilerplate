f8b8d5c49a54be0abc59e2640e6538ed6ef109f4
Jaan Lambert <jaan.lambert@gmail.com>
2016-03-11 14:22:57 +0200

    Redirect to dashboard after login

--- app/sagas/sagas.js
+++ app/sagas/sagas.js
@@ -2,11 +2,13 @@ import { takeEvery } from 'redux-saga'
 import { call, put } from 'redux-saga/effects'
 import * as types from '../constants/ActionTypes.js'
 import { login } from '../api/Api.js'
+import { push } from 'react-router-redux'
 
 function* doLogin(action) {
   try {
     const user = yield call(login, action.details)
     yield put({ type: types.LOGIN_SUCCEEDED, user: action.details })
+    yield put(push('/dashboard'))
   } catch(e) {
     yield put({ type: types.LOGIN_FAILED, message: e.message })
   }
